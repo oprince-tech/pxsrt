39be7b21b9feeb485c418f73d1ba6a307bd51603
Oliver Prince <oprince.dev@gmail.com>
2017-01-24 15:10:23 -0600

    Now saves using the same file extension user provides

--- save.py
+++ save.py
@@ -3,11 +3,10 @@ import os, args
 
 def save(output):
     sub_path        = './images/export/'
-    file_extension  = '.jpg'
-
     if not os.path.exists(sub_path):
         os.makedirs(sub_path)
-    base = os.path.splitext(os.path.basename(args.input_image))[0]
+        
+    base, file_extension = os.path.splitext(os.path.basename(args.input_image))
     output_base = base + '_' + str(args.mode) + str(args.direction) + str(args.threshold) + str(args.reverse) + str(args.upper) + str(file_extension)
     output_path = ('/').join(args.input_image.split('/')[:-1])
     output_file = output_path + sub_path + output_base
