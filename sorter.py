daaabf2d12180345f76df5301df3f6fb434d7d3a
oli <oli@asio.mshome.net>
2020-05-25 20:24:46 -0500

    Added median pivot functionality to improve quick sorting.

--- sorter.py
+++ sorter.py
@@ -1,12 +1,30 @@
 from PIL import Image
 import args
+from statistics import median #to find median pivot
+
+def find_pivot(cache):
+    clen = int(len(cache))
+    a = sum(cache[0])
+    b = sum(cache[int(clen/2)])
+    c = sum(cache[-1])
+    if a <= b and b <= c:
+        return int(clen/2)
+    if b <= a and c <= b:
+        return int(clen/2)
+    if a <= c and c <= b:
+        return -1
+    if c <= a and b <= c:
+        return -1
+    else:
+        return 0
 
 
 def quick_sort(cache):
     if cache == []:
         return cache
     else:
-        p = cache[0]
+        pivot = find_pivot(cache)
+        p = cache[int(pivot)]
         if args.reverse:
             l = quick_sort([x for x in cache[1:] if (x[0]+x[1]+x[2]) >= (p[0]+p[1]+p[2])])
             r = quick_sort([x for x in cache[1:] if (x[0]+x[1]+x[2]) < (p[0]+p[1]+p[2])])
